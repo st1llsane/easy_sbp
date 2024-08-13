6388f3971b3dcf7a4a65ed6ca29c720200c8fa6e
corex <corex@MacBook-Air-corex.local>
2024-08-13 18:02:57 +0200

    feat: working on styles and search for bank

--- example/lib/widgets/bank_list.dart
+++ example/lib/widgets/bank_list.dart
@@ -10,115 +10,288 @@ class BankList extends StatefulWidget {
 }
 
 class _BankListState extends State<BankList> {
+  late TextEditingController searchController;
+  final List<Bank> bankList = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+  }
+
+  Future<List<Bank>> loadBankList() async {
+    final bankList = await getBankList();
+
+    setState(() {
+      isLoading = false;
+    });
+
+    return bankList;
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: ValueListenableBuilder(
-      //     valueListenable: _statesMapNotifier,
-      //     builder: (_, statesMap, __) {
-      //       final wasRestarted = statesMap['wasRestarted'] ?? false;
-      //       final wasTransited = statesMap['wasTransited'] ?? false;
-      //       return Column(
-      //         crossAxisAlignment: CrossAxisAlignment.start,
-      //         children: [
-      //           Text(
-      //             'Свернуто и открыто: $wasRestarted',
-      //             style: const TextStyle(fontSize: 18),
-      //           ),
-      //           Text(
-      //             'Переход в банк: $wasTransited',
-      //             style: const TextStyle(fontSize: 18),
-      //           ),
-      //         ],
-      //       );
-      //     },
-      //   ),
-      // ),
       body: SafeArea(
-        child: FutureBuilder<List<Bank>>(
-          future: getBankList(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: Colors.grey.shade500,
-                ),
-              );
-            }
-
-            late final data = snapshot.data ?? [];
-
-            if (data.isEmpty) {
-              return const Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      'Возникла ошибка при попытке получить список банков',
-                      style: TextStyle(fontSize: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(
+                      Icons.arrow_back_ios_rounded,
+                      size: 20,
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: getBankList,
-                      child: Text(
-                        'Повторить попытку',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                    )
-                  ],
-                ),
-              );
-            }
-
-            return ListView.separated(
-              itemCount: data.length,
-              separatorBuilder: (_, __) => Divider(
-                color: Colors.grey.shade300,
-                height: 1,
+                    // onPressed: () {
+                    //   Navigator.of(context).pop();
+                    // },
+                    onPressed: () => print('Back'),
+                  ),
+                  const SizedBox(width: 5),
+                  const Text(
+                    'Оплата через СБП',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    maxLines: 2,
+                  ),
+                ],
               ),
-              itemBuilder: (context, index) {
-                final bank = data[index];
-
-                return InkWell(
-                  // onTap: () => _openBank(context, schema: bank.schema),
-                  onTap: () => (),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 18,
-                      vertical: 14,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Flexible(
-                          child: Row(
-                            children: [
-                              Image.network(
-                                bank.logoURL,
-                                height: 50,
-                                width: 50,
+            ),
+            if (!isLoading) ...[
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  bottom: 20,
+                ),
+                child: SearchBar(
+                  onChanged: (value) => print(value),
+                  hintText: 'Введите название банка',
+                  shadowColor: null,
+                  elevation: const WidgetStatePropertyAll(0),
+                  hintStyle: const WidgetStatePropertyAll(TextStyle(
+                    fontSize: 20,
+                  )),
+                  textStyle: const WidgetStatePropertyAll(TextStyle(
+                    fontSize: 20,
+                  )),
+                  keyboardType: TextInputType.text,
+                  controller: searchController,
+                ),
+              ),
+            ],
+            Expanded(
+              child: Builder(
+                builder: (BuildContext context) {
+                  if (isLoading) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.grey.shade500,
+                      ),
+                    );
+                  }
+                  if (!isLoading && bankList.isEmpty) {
+                    return Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Text(
+                              'Возникла ошибка при попытке получить список банков',
+                              style: TextStyle(fontSize: 20),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 20),
+                            ElevatedButton.icon(
+                              onPressed: getBankList,
+                              label: const Text(
+                                'Повторить попытку',
+                                style: TextStyle(fontSize: 20),
                               ),
-                              const SizedBox(width: 10),
-                              Flexible(
-                                child: Text(
-                                  bank.bankName,
-                                  style: const TextStyle(fontSize: 18),
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
+                              icon: const Icon(
+                                Icons.replay_outlined,
+                              ),
+                              style: ButtonStyle(
+                                backgroundColor: WidgetStateProperty.all<Color>(
+                                  Colors.transparent,
                                 ),
+                                elevation: const WidgetStatePropertyAll(0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }
+
+                  return ListView.separated(
+                    itemCount: bankList.length,
+                    separatorBuilder: (_, __) => Divider(
+                      color: Colors.grey.shade300,
+                      height: 1,
+                    ),
+                    itemBuilder: (context, index) {
+                      final bank = bankList[index];
+
+                      return InkWell(
+                        onTap: () => (),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 18,
+                            vertical: 14,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                child: Row(
+                                  children: [
+                                    Image.network(
+                                      bank.logoURL,
+                                      height: 50,
+                                      width: 50,
+                                    ),
+                                    const SizedBox(width: 10),
+                                    Flexible(
+                                      child: Text(
+                                        bank.bankName,
+                                        style: const TextStyle(fontSize: 20),
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const Icon(
+                                Icons.arrow_forward_ios_rounded,
+                                size: 18,
                               ),
                             ],
                           ),
                         ),
-                        const Icon(Icons.arrow_forward_ios_rounded, size: 18),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
-          },
+                      );
+                    },
+                  );
+                },
+              ),
+              // child: builder<List<Bank>>(
+              //   future: loadBankList(),
+              //   builder: (context, snapshot) {
+              //     if (snapshot.connectionState == ConnectionState.waiting) {
+              //       return Center(
+              //         child: CircularProgressIndicator(
+              //           color: Colors.grey.shade500,
+              //         ),
+              //       );
+              //     }
+
+              //     late final data = snapshot.data ?? [];
+
+              //     if (data.isEmpty) {
+              //       return Padding(
+              //         padding: const EdgeInsets.all(20),
+              //         child: Center(
+              //           child: Column(
+              //             mainAxisSize: MainAxisSize.min,
+              //             children: [
+              //               const Text(
+              //                 'Возникла ошибка при попытке получить список банков',
+              //                 style: TextStyle(fontSize: 20),
+              //                 textAlign: TextAlign.center,
+              //               ),
+              //               const SizedBox(height: 20),
+              //               ElevatedButton.icon(
+              //                 onPressed: getBankList,
+              //                 label: const Text(
+              //                   'Повторить попытку',
+              //                   style: TextStyle(fontSize: 20),
+              //                 ),
+              //                 icon: const Icon(
+              //                   Icons.replay_outlined,
+              //                 ),
+              //                 style: ButtonStyle(
+              //                   backgroundColor: WidgetStateProperty.all<Color>(
+              //                     Colors.transparent,
+              //                   ),
+              //                   elevation: const WidgetStatePropertyAll(0),
+              //                 ),
+              //               ),
+              //             ],
+              //           ),
+              //         ),
+              //       );
+              //     }
+
+              //     isLoadingOrEmpty = false;
+
+              //     return ListView.separated(
+              //       itemCount: data.length,
+              //       separatorBuilder: (_, __) => Divider(
+              //         color: Colors.grey.shade300,
+              //         height: 1,
+              //       ),
+              //       itemBuilder: (context, index) {
+              //         final bank = data[index];
+
+              //         return InkWell(
+              //           onTap: () => (),
+              //           child: Padding(
+              //             padding: const EdgeInsets.symmetric(
+              //               horizontal: 18,
+              //               vertical: 14,
+              //             ),
+              //             child: Row(
+              //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //               children: [
+              //                 Flexible(
+              //                   child: Row(
+              //                     children: [
+              //                       Image.network(
+              //                         bank.logoURL,
+              //                         height: 50,
+              //                         width: 50,
+              //                       ),
+              //                       const SizedBox(width: 10),
+              //                       Flexible(
+              //                         child: Text(
+              //                           bank.bankName,
+              //                           style: const TextStyle(fontSize: 20),
+              //                           maxLines: 1,
+              //                           overflow: TextOverflow.ellipsis,
+              //                         ),
+              //                       ),
+              //                     ],
+              //                   ),
+              //                 ),
+              //                 const Icon(
+              //                   Icons.arrow_forward_ios_rounded,
+              //                   size: 18,
+              //                 ),
+              //               ],
+              //             ),
+              //           ),
+              //         );
+              //       },
+              //     );
+              //   },
+              // ),
+            ),
+          ],
         ),
       ),
     );
