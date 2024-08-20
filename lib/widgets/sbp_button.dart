8296a34c2e9a3e77b7fbe563ce3a849bc4c6a941
st1llsane <st1llsane@MacBook-Pro>
2024-08-20 13:25:30 -0700

    feat: added isLoading and isDisabled states to button

--- lib/widgets/sbp_button.dart
+++ lib/widgets/sbp_button.dart
@@ -7,15 +7,19 @@ import 'package:flutter/scheduler.dart';
 class ESbpButton extends StatefulWidget {
   final Function() onPressed;
   final List<String>? bankSchemesToLoad;
+  final bool isHandleLifecycle;
+  final bool isLoading;
+  final bool isDisabled;
   final ESbpButtonTheme sbpButtonTheme;
   final ESbpModalTheme sbpModalTheme;
-  final bool isHandleLifecycle;
 
   const ESbpButton({
     super.key,
     required this.onPressed,
     this.bankSchemesToLoad,
     this.isHandleLifecycle = false,
+    this.isLoading = false,
+    this.isDisabled = false,
     this.sbpButtonTheme = const ESbpButtonTheme(),
     this.sbpModalTheme = const ESbpModalTheme(),
   });
@@ -72,16 +76,17 @@ class _ESbpButtonState extends State<ESbpButton> {
       height: widget.sbpButtonTheme.height,
       child: ElevatedButton(
         onPressed: () {
-          if (!isAppResumed) {
+          if (!isAppResumed || widget.isLoading || widget.isDisabled) {
             return;
           }
 
           widget.onPressed();
         },
         style: ElevatedButton.styleFrom(
-          backgroundColor: isAppResumed
-              ? widget.sbpButtonTheme.bgColor
-              : widget.sbpButtonTheme.bgColor.withOpacity(.6),
+          backgroundColor:
+              isAppResumed && !widget.isLoading && !widget.isDisabled
+                  ? widget.sbpButtonTheme.bgColor
+                  : widget.sbpButtonTheme.bgColor.withOpacity(.6),
           foregroundColor: widget.sbpButtonTheme.fgColor,
           surfaceTintColor: widget.sbpButtonTheme.bgColor,
           shadowColor: Colors.transparent,
@@ -95,11 +100,11 @@ class _ESbpButtonState extends State<ESbpButton> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: isAppResumed
+          children: isAppResumed && !widget.isLoading
               ? [
                   if (widget.sbpButtonTheme.isShowIcon) ...[
                     Opacity(
-                      opacity: isAppResumed ? 1 : .6,
+                      opacity: isAppResumed && !widget.isDisabled ? 1 : .6,
                       child: Image.asset(
                         'assets/sbp_logo.png',
                         package: 'easy_sbp',
@@ -112,7 +117,7 @@ class _ESbpButtonState extends State<ESbpButton> {
                   Text(
                     "СБП",
                     style: TextStyle(
-                      color: isAppResumed
+                      color: isAppResumed && !widget.isDisabled
                           ? widget.sbpButtonTheme.fgColor
                           : widget.sbpButtonTheme.fgColor.withOpacity(.6),
                       fontSize: 18,
